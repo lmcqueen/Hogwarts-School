85b719e68f99459d0fb6d6dbc08d88f949f2f403
User <User@10.18.148.130>
2015-10-08 09:49:47 -0600

    October 8 - House class added and tested

---
+++ HogwartSchool/src/byui/cit260/hogwartsschool/model/House.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hogwartsschool.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author lmcqueen
+ */
+public class House implements Serializable {
+    
+    private String name;
+    private String description;
+    private int points;
+
+    public House() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.name);
+        hash = 83 * hash + Objects.hashCode(this.description);
+        hash = 83 * hash + this.points;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final House other = (House) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (this.points != other.points) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "House{" + "name=" + name + ", description=" + description + ", points=" + points + '}';
+    }
+    
+    
+}
--- HogwartSchool/src/hogwartsschool/HogwartsSchool.java
+++ HogwartSchool/src/hogwartsschool/HogwartsSchool.java
@@ -8,6 +8,7 @@ package hogwartsschool;
 import byui.cit260.hogwartsschool.model.Game;
 import byui.cit260.hogwartsschool.model.Player;
 import byui.cit260.hogwartsschool.model.Character;
+import byui.cit260.hogwartsschool.model.House;
 /**
  *
  * @author lmcqueen and jagiriga
@@ -18,14 +19,14 @@ public class HogwartsSchool {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //implement Game instance
         Game gameOne = new Game();
-        
         gameOne.setStartTime(9.43);
         
         String gameInfo = gameOne.toString();
         System.out.println(gameInfo);
         
+        //implement Player instance
         Player playerOne = new Player();
         playerOne.setName("Bob");
         playerOne.setCurrentRow(3);
@@ -34,6 +35,7 @@ public class HogwartsSchool {
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
         
+        //implement Character instance
         Character characterOne = new Character();
         characterOne.setName("George");
         characterOne.setDescription("A character named George");
@@ -42,6 +44,15 @@ public class HogwartsSchool {
         
         String characterInfo = characterOne.toString();
         System.out.println(characterInfo);
+        
+        //implement House instance
+        House houseOne = new House();
+        houseOne.setName("Ravenclaw");
+        houseOne.setDescription("The smart house");
+        houseOne.setPoints(500);
+        
+        String houseInfo = houseOne.toString();
+        System.out.println(houseInfo);
     }
     
 }

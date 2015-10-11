a0138abd17438c3c283e22162f114005ff642cba
Johnson <Johnson@Agiriga>
2015-10-10 21:29:54 -0700

    Oct.8 - Added and tested Inventory class

---
+++ HogwartSchool/src/byui/cit260/hogwartsschool/model/Cauldron.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hogwartsschool.model;
+
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author Johnson
+ */
+public class Cauldron extends InventoryItem implements Serializable{
+    // class instance variable
+    private int depth;
+    private int diameter; 
+
+    public Cauldron() {
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + this.depth;
+        hash = 97 * hash + this.diameter;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cauldron other = (Cauldron) obj;
+        if (this.depth != other.depth) {
+            return false;
+        }
+        if (this.diameter != other.diameter) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cauldron{" + "name=" + this.getName() + ", description=" + this.getDescription() + ", itemType=" + this.getItemType() + ", quantity=" + this.getQuantity() + ", depth=" + depth + ", diameter=" + diameter + '}';
+    }
+    
+             
+    
+}
---
+++ HogwartSchool/src/byui/cit260/hogwartsschool/model/Location.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hogwartsschool.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author jagiriga
+ */
+public class Location implements Serializable{
+    
+    // class instance variables
+    private String name;
+    private int row;
+    private int column;
+
+    public Location() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.name);
+        hash = 47 * hash + this.row;
+        hash = 47 * hash + this.column;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.row != other.row) {
+            return false;
+        }
+        if (this.column != other.column) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "name=" + name + ", row=" + row + ", column=" + column + '}';
+    }
+    
+    
+}

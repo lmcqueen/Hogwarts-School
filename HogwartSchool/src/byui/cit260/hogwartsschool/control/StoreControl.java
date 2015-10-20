d8bc31e7d44b410714cf245615288259756ce27e
User <User@10.18.151.46>
2015-10-20 09:45:34 -0600

    October 20 - Added Control package and classes. Prepared apprentMagnitude function.

---
+++ HogwartSchool/src/byui/cit260/hogwartsschool/control/StoreControl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hogwartsschool.control;
+
+/**
+ *
+ * @author User
+ */
+public class StoreControl {
+    
+    public double appartentMagnitude (double absoluteMagnitude, double earthDistance)
+    {
+    }
+    
+}

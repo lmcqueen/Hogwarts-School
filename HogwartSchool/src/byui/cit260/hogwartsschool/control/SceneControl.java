dfea996c21486bb559b4b17c26eb515abf7220f5
User <User@10.17.163.97>
2015-10-23 13:56:22 -0600

    Oct 23 - Comments Added

--- HogwartSchool/src/byui/cit260/hogwartsschool/control/SceneControl.java
+++ HogwartSchool/src/byui/cit260/hogwartsschool/control/SceneControl.java
@@ -4,14 +4,14 @@
  * and open the template in the editor.
  */
 package byui.cit260.hogwartsschool.control;
-import java.lang.Math;
 
 /**
  *
- * @author User
+ * @author lmcqueen and Johnson
  */
 public class SceneControl {
 
+   //Group assignmnet
    public double appartentMagnitude (double magnitude, double distance){
    
        if (magnitude > 50 || magnitude < -50) {
@@ -29,6 +29,7 @@ public class SceneControl {
 
    }
    
+   //Leah McQueen Individual Assignemnt
    public double gallonsCauldronHolds (double diameter, double depth) {
        
         if (diameter < 1 || depth < 1) {

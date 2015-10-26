c7ae8cbfd5795ad5459adb718ccab4f2dac1dc61
User <User@10.18.149.30>
2015-10-26 09:55:10 -0600

    Oct 26 - Main function updated. View package and start program class added.

---
+++ HogwartSchool/src/byui/cit260/hogwartsschool/view/StartProgramView.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hogwartsschool.view;
+
+/**
+ *
+ * @author lmcqueen and Johnson
+ */
+public class StartProgramView {
+    
+    /**
+     * startProgram(): void
+     * BEGIN
+     *  Display the banner screen
+     *  Get the players name
+     *  Create a new player
+     *  DISPLAY a customized welcome message
+     *  DISPLAY the main menu
+     * END 
+     */
+    
+    public StartProgramView(){
+    }
+    
+    public void startProgram(){
+        this.displayBanner();
+    }
+    
+    public void displayBanner(){
+        
+    }
+            
+}
--- HogwartSchool/src/hogwartsschool/HogwartsSchool.java
+++ HogwartSchool/src/hogwartsschool/HogwartsSchool.java
@@ -5,17 +5,9 @@
  */
 package hogwartsschool;
 
-import byui.cit260.hogwartsschool.model.Game;
-import byui.cit260.hogwartsschool.model.Player;
-import byui.cit260.hogwartsschool.model.Character;
-import byui.cit260.hogwartsschool.model.Classroom;
-import byui.cit260.hogwartsschool.model.Course;
-import byui.cit260.hogwartsschool.model.Hallway;
-import byui.cit260.hogwartsschool.model.House;
-import byui.cit260.hogwartsschool.model.InventoryItem;
-import byui.cit260.hogwartsschool.model.Notebook;
-/**
- *
+import byui.cit260.hogwartsschool.view.StartProgramView;
+
+ /**
  * @author lmcqueen and jagiriga
  */
 public class HogwartsSchool {
@@ -24,82 +16,9 @@ public class HogwartsSchool {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //implement Game instance
-        Game gameOne = new Game();
-        gameOne.setStartTime(9.43);
         
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-        
-        //test Player instance
-        Player playerOne = new Player();
-        playerOne.setName("Bob");
-        playerOne.setCurrentRow(3);
-        playerOne.setCurrentCol(6);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        //test Character instance
-        Character characterOne = new Character();
-        characterOne.setName("George");
-        characterOne.setDescription("A character named George");
-        characterOne.setCurrentRow(5);
-        characterOne.setCurrentCol(3);
-        
-        String characterInfo = characterOne.toString();
-        System.out.println(characterInfo);
-        
-        //test House instance
-        House houseOne = new House();
-        houseOne.setName("Ravenclaw");
-        houseOne.setDescription("The smart house");
-        houseOne.setPoints(500);
-        
-        String houseInfo = houseOne.toString();
-        System.out.println(houseInfo);
-        
-        //testing InventoryItem class
-        InventoryItem itemOne = new InventoryItem();
-        itemOne.setName("john");
-        itemOne.setDescription("john is a person");
-        itemOne.setItemType("humanbeing");
-        itemOne.setQuantity(2);
-        
-        String itemOneInfo = itemOne.toString();
-        System.out.println(itemOneInfo);
-        
-        //test Course instance
-        Course courseOne = new Course();
-        courseOne.setName("Potions");
-        courseOne.setDescription("A course that teaches potions");
-        courseOne.setTeacher("Snape");
-        
-        String courseOneInfo = courseOne.toString();
-        System.out.println(courseOneInfo);
-        
-        //test note instance
-        Notebook noteOne = new Notebook();
-        noteOne.setNotes("This is a note");
-        
-        String noteOneInfo = noteOne.toString();
-        System.out.println(noteOneInfo);
-        
-        //test Hallway
-        Hallway hallwayOne = new Hallway();
-        hallwayOne.setDesctiption("This hallway has a ghost");
-        
-        String hallwayOneInfo = hallwayOne.toString();
-        System.out.println(hallwayOneInfo);
-        
-        //test classroom
-        Classroom classOne = new Classroom();
-        classOne.setBlocked(true);
-        classOne.setDescription("The potions classroom");
-        classOne.setVisited(false);
-        
-        String classOneInfo = classOne.toString();
-        System.out.println(classOneInfo);
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.startProgram();
         
     }
 }

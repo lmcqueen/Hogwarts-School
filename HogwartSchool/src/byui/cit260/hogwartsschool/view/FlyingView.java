76c2bdbfdb8ed762df70bfa44056f0a7f218a345
Johnson <Johnson@Agiriga>
2015-11-06 22:40:47 -0800

    November 6. Night changes.

--- HogwartSchool/src/byui/cit260/hogwartsschool/view/FlyingView.java
+++ HogwartSchool/src/byui/cit260/hogwartsschool/view/FlyingView.java
@@ -5,7 +5,6 @@
  */
 package byui.cit260.hogwartsschool.view;
 
-import byui.cit260.hogwartsschool.control.SceneControl;
 import java.util.Scanner;
 
 /**
@@ -13,82 +12,58 @@ import java.util.Scanner;
  * @author Johnson
  */
 public class FlyingView {
-
+       
     public void displayMenu() {
-        //DISPLAY description of the add water function
-        System.out.println("Wellcome to flying menu veiw");
-        System.out.println("MENU");
-        
-        Scanner input= new Scanner(System.in);
+        char selection = ' ';
+        do {
+            System.out.println("Welcome to flying menu veiw");
+            System.out.println("enter a String");
+            Scanner input= new Scanner(System.in);
         
         System.out.print("Please enter distance: ");
         int distance = input.nextInt();
         
         System.out.print("Please enter time: ");
         int time = input.nextInt();
-    
-        //GET cauldron depth and diameter
-       // double distance = this.getInput(); 
-       // double time = this.getInput(); 
-        
-        
-        if ( distance == -1 ||  time == -1){
-            System.out.println(" Invalid input, try again.");
-        }
-        else{
-            //Perform calculation
-            //DISPLAY result
+            
             this.doAction(distance, time);
-        }
-      
+        } while (selection != 'E');      
     }
-
-   
-    void getInput() {
-        boolean valid = false;
-        Scanner keyboard = new Scanner(System.in);
-        double value = -1;
+    public String getInput() {
         
-        //WHILE a valid value has not been entered
-        while(!valid){
-            // DISPLAY a message prompting the user to enter a value
-            System.out.println("Please enter distance" );
+        Scanner keyboard = new Scanner(System.in);
+        boolean valid = false;
+        String selection = null;
+        
+        while (!valid){
             
-            //GET the value entered from keyboard 
-            int distance = keyboard.nextInt();
-            System.out.println("Please enter time");
-            int time = keyboard.nextInt();
-       
-                    
-            //IF input is a number THEN Convert the string to a double
-            if (distance > 1 || time > 1){
+            selection = keyboard.nextLine();
+            selection = selection.trim();
             
-                 String = Double.parseDouble();
-                 Doubl = Double.parseDouble();
-
+            if (selection.length()<1){
+                System.out.println("\n*** Invalid selection ***Try again");
             }
-            //ELSE IF the user did not input a value greater or equal to one THEN DISPLAY an invalid message and CONTINUE
-            else{
-                System.out.println(". Enter a number greater or equal to one.");
-                continue;
-            }
-            
-            //IF the user did not enter a number THEN DISPLAY an invalid input message
-            if(value < 1){
-                System.out.println(". Enter a number greater than one. ***");
-                continue;
-            }
-            
             break;
         }
-        
-        return value;
+        return selection;
     }
+        
 
     private void doAction(double distance, double time) {
+        //create instance
+        FlyingMenuView myMenu = new FlyingMenuView();
+           
         //Perform calculation by calling control function
         double averageAcceleration = mySceneControl.averageAcceleration(distance, time);
         
+                    //IF input is a number THEN Convert the string to a double
+            if (distance > 1 || time > 1){
+            
+                distance = Double.parseDouble(String value);
+                 time = Double.parseDouble(String value);
+
+            }
+
         //DISPLAY result
         System.out.println("Good job, please move on");
     }
@@ -104,3 +79,4 @@ public class FlyingView {
     }
     
 }
+ 

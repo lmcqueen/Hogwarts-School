9a98e586f9a04815b87be41d62f9af6c5b6d26fb
Johnson <Johnson@Agiriga>
2015-12-04 10:10:56 -0800

    December 4. Created the inventoryPrintView, still working on it.

---
+++ HogwartSchool/src/byui/cit260/hogwartsschool/view/InventoryPrintView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hogwartsschool.view;
+
+/**
+ *
+ * @author Johnson
+ */
+public class InventoryPrintView extends View {
+
+    public InventoryPrintView(String promptMessage) {
+        super(promptMessage);
+        // prompt message of pls enter your input
+    }
+
+    @Override
+    public boolean doAction(Object obj) {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

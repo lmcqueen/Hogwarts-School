fbf7e885b8715b82ed16f4a01eaf2e1836dafe32
Johnson <Johnson@Agiriga>
2015-12-16 11:13:57 -0800

    December 16. TransfigurationView functions written with Brother Jackson help.

--- HogwartSchool/src/byui/cit260/hogwartsschool/view/TransfigurationView.java
+++ HogwartSchool/src/byui/cit260/hogwartsschool/view/TransfigurationView.java
@@ -5,38 +5,63 @@
  */
 package byui.cit260.hogwartsschool.view;
 
+import java.util.Random;
+
 /**
  *
  * @author Johnson
  */
 public class TransfigurationView extends View {
-    
+    int requiredWands;
+    int requiredIncantations;
+    int wand;
+    int incantation;
+
+    public TransfigurationView(String promptMessage) {
+        super(promptMessage);
+    }
+
+
     public TransfigurationView() {
         super("\n------------------------------------------------"
-        + "\n|               Transfiguration Menu                      |"
-        + "\n------------------------------------------------"
-        + "\nI - Instructions" 
-        + "\nW – Wave wand"
-        + "\nC - Incantation"
-        + "\nF - Finish"
-        + "\nQ - Quit"
-        + "\n-----------------------------------------");
+                + "\n|               Transfiguration Menu                      |"
+                + "\n------------------------------------------------"
+                + "\nI - Instructions"
+                + "\nW – Wave wand"
+                + "\nC - Incantation"
+                + "\nQ - Quit"
+                + "\n-----------------------------------------");
+        
+        
+        // generate random values for number warns needed and incantations needed
+        
+        Random rn = new Random();
+        requiredWands = rn.nextInt(5);
+        
+        rn = new Random();
+        requiredIncantations = rn.nextInt(5);
+       // set the number warnds selected and incantations selected to zero's
+        wand = 0;
+        incantation = 0;
+        
+               
+        
     }
 
     @Override
     public boolean doAction(Object obj) {
-        
+
         String value = (String) obj;
         value = value.toUpperCase();
-        if(value.length() > 1){
+        if (value.length() > 1) {
             ErrorView.display(this.getClass().getName(), "\n *** Invalid Selection. Enter only a single character. ***\n");
             return false;
         }
 
         char choice = value.charAt(0);
-        
+
         switch (choice) {
-            case 'I': 
+            case 'I':
                 this.displayInstructions();
                 break;
             case 'W':
@@ -45,45 +70,47 @@ public class TransfigurationView extends View {
             case 'C':
                 this.displayIncantation();
                 break;
-            case 'F':
-                this.displayFinish();
-                break;
+           
             case 'Q':
                 return true;
             default:
                 this.console.println("\n*** Invalid selection. Try again. ***");
-                break;  
-        } 
-        
+                break;
+        }
+
         return false;
-   }
+    }
 
     private void displayInstructions() {
         this.console.println("To turn a match into a niddle, you need to wave a wand two times and doThis is a charm that transform one thing into an entirely different thing");
     }
 
-    private void displayWaveWand () {
+    private void displayWaveWand() {
         this.console.println("Used this burton to wave a wand ");
+        wand = wand+1;
         
+
     }
 
-    private void displayIncantation () {
-        this.console.println("Used this burtton for incantation ");}
+    private boolean displayIncantation() {
+        this.console.println("Used this burtton for incantation ");
+        incantation++;
         
+        if (requiredWands == wand && requiredIncantations == incantation){
+            return true;
         
-    private void displayFinish() {
-        // will have an arraylist that will collect
-        // check the arraylist, make sure they are of the same sixe. If not, made an error
-        // check if the user type the things in the right order
-            // do whatever message if they get right or wrong
-                // a code to reset the arraylist at the very end of everything
-    }
-        
-
-        
-
-
-    
+        else{ 
+                
 }
+}
+        return false;
+    }
+}
+  // will have an arraylist that will collect
+                // check the arraylist, make sure they are of the same sixe. If not, made an error
+                // check if the user type the things in the right order
+                // do whatever message if they get right or wrong
+                // a code to reset the arraylist at the very end of everything
+//}
 
-    
+        
